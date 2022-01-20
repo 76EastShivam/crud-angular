9b4f183d27994e95b174a173152014bdf7591463
Shivayashiv <94154781+Shivayashiv@users.noreply.github.com>
2022-01-20 20:59:51 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './component/add-tutorial/add-tutorial.component';
+import {AngularFirestoreModule} from './'
+import { TutorialsListComponent } from './component/tutorials-list/tutorials-list.components';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // for firestore
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
